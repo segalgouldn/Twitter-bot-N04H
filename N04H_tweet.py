a081b5bf4096f52793a839699fc0c3c83e2d715c
Noah Segal-Gould <ns2349@bard.edu>
2017-07-28 21:02:57 -0400

    Update N04H_tweet.py

--- N04H_tweet.py
+++ N04H_tweet.py
@@ -5,10 +5,10 @@ import tweepy
 import subprocess
 
 
-consumer_key = 'INSERT'
-consumer_secret = 'INSERT'
-access_token = 'INSERT'
-access_token_secret = 'INSERT'
+consumer_key = 'INSERT_YOUR_OWN'
+consumer_secret = 'INSERT_YOUR_OWN'
+access_token = 'INSERT_YOUR_OWN'
+access_token_secret = 'INSERT_YOUR_OWN'
 
 tweets_file = open("noah_tweets.txt", encoding="utf8")
 tweets_text = tweets_file.read()
@@ -27,6 +27,7 @@ def random_sublist(lst, length):
 
 def tweet(tweets_list):
     for i in range(10000):
+        print("********** START LOOP **********")
         selected_original_tweet = choice(tweets_list)
         selected_original_tweet_words = selected_original_tweet.split()
         words_length = len(selected_original_tweet_words)
@@ -34,15 +35,19 @@ def tweet(tweets_list):
             selected_original_start = b" ".join(random_sublist(selected_original_tweet_words, 4))
         elif words_length > 3:
             selected_original_start = b" ".join(random_sublist(selected_original_tweet_words, 3))
-        elif words_length > 2:
+        elif words_length >= 2:
             selected_original_start = b" ".join(random_sublist(selected_original_tweet_words, 2))
         elif words_length == 1:
-            selected_original_start = selected_original_tweet_words[0].encode()
+            selected_original_start = selected_original_tweet_words[0]
         else:
-            selected_original_start = choice(list(b'abcdefghijklmnopqrstuvwxyz'))
-        
+            selected_original_start = choice(list('abcdefghijklmnopqrstuvwxyz')).encode()
+        print("Original Selection:")
+        print(selected_original_tweet)
+        print("Words in Original Selection:")
+        print(selected_original_tweet_words)
+        print("Final Selected Start")
+        print(selected_original_start)
         desired_tweet_length = abs(randint(40, 140) - len(selected_original_start))
-        # print(selected_original_start)
         
         if selected_original_start == b"":
             command = b"th sample.lua -checkpoint train_44000.t7 -length \"" + str(desired_tweet_length).encode() + b"\" -temperature 0.25 -gpu -1"
@@ -59,6 +64,9 @@ def tweet(tweets_list):
             continue
         
         api.update_status(final_tweet)
+        print("Final Output")
+        print(final_tweet)
+        print("********** End Loop **********")
         sleep(1800)  # Tweet 48 times per day.
 
 
